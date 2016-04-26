22cabed0f7dc841ad5143e7f96408b70b9b7ec96
Peter Zeller <p_zeller@cs.uni-kl.de>
2016-04-26 19:01:37 +0200

    auto format code

--- src/java_cup/runtime/Scanner.java
+++ src/java_cup/runtime/Scanner.java
@@ -5,8 +5,8 @@ package java_cup.runtime;
  * implementation of <code>lr_parser.scan()</code>.  Integration
  * of scanners implementing <code>Scanner</code> is facilitated.
  *
- * @version last updated 23-Jul-1999
  * @author David MacMahon <davidm@smartsc.com>
+ * @version last updated 23-Jul-1999
  */
 
 /* *************************************************
@@ -20,6 +20,8 @@ package java_cup.runtime;
   <code>null</code>.
  ***************************************************/
 public interface Scanner {
-    /** Return the next token, or <code>null</code> on end-of-file. */
+    /**
+     * Return the next token, or <code>null</code> on end-of-file.
+     */
     public Symbol next_token() throws java.lang.Exception;
 }
--- src/java_cup/runtime/Symbol.java
+++ src/java_cup/runtime/Symbol.java
@@ -2,11 +2,11 @@ package java_cup.runtime;
 
 /**
  * Defines the Symbol class, which is used to represent all terminals
- * and nonterminals while parsing.  The lexer should pass CUP Symbols 
+ * and nonterminals while parsing.  The lexer should pass CUP Symbols
  * and CUP returns a Symbol.
  *
+ * @author Frank Flannery
  * @version last updated: 7/3/96
- * @author  Frank Flannery
  */
 
 /* ****************************************************************
@@ -26,90 +26,100 @@ public class Symbol {
 
 //  TUM 20060327: Added new Constructor to provide more flexible way
 //   for location handling
-/*******************************
- *******************************/
-    public Symbol(int id, Symbol left, Symbol right, Object o){
-        this(id,left.left,right.right,o);
+
+    /*******************************
+     *******************************/
+    public Symbol(int id, Symbol left, Symbol right, Object o) {
+        this(id, left.left, right.right, o);
     }
-    public Symbol(int id, Symbol left, Symbol right){
-        this(id,left.left,right.right);
+
+    public Symbol(int id, Symbol left, Symbol right) {
+        this(id, left.left, right.right);
     }
-    public Symbol(int id, Symbol left, Object o){
-        this(id,left.right,left.right,o);
+
+    public Symbol(int id, Symbol left, Object o) {
+        this(id, left.right, left.right, o);
     }
- /*******************************
-  Constructor for l,r values
- *******************************/
 
-  public Symbol(int id, int l, int r, Object o) {
-    this(id);
-    left = l;
-    right = r;
-    value = o;
-  }
+    /*******************************
+     * Constructor for l,r values
+     *******************************/
 
-/*******************************
-  Constructor for no l,r values
-********************************/
+    public Symbol(int id, int l, int r, Object o) {
+        this(id);
+        left = l;
+        right = r;
+        value = o;
+    }
 
-  public Symbol(int id, Object o) {
-    this(id, -1, -1, o);
-  }
+    /*******************************
+     * Constructor for no l,r values
+     ********************************/
 
-/*****************************
-  Constructor for no value
-  ***************************/
+    public Symbol(int id, Object o) {
+        this(id, -1, -1, o);
+    }
 
-  public Symbol(int id, int l, int r) {
-    this(id, l, r, null);
-  }
+    /*****************************
+     * Constructor for no value
+     ***************************/
 
-/***********************************
-  Constructor for no value or l,r
-***********************************/
+    public Symbol(int id, int l, int r) {
+        this(id, l, r, null);
+    }
 
-  public Symbol(int sym_num) {
-    this(sym_num, -1);
-    left = -1;
-    right = -1;
-  }
+    /***********************************
+     * Constructor for no value or l,r
+     ***********************************/
 
-/***********************************
-  Constructor to give a start state
-***********************************/
-  Symbol(int sym_num, int state)
-    {
-      sym = sym_num;
-      parse_state = state;
+    public Symbol(int sym_num) {
+        this(sym_num, -1);
+        left = -1;
+        right = -1;
+    }
+
+    /***********************************
+     * Constructor to give a start state
+     ***********************************/
+    Symbol(int sym_num, int state) {
+        sym = sym_num;
+        parse_state = state;
     }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
-  /** The symbol number of the terminal or non terminal being represented */
-  public int sym;
+    /**
+     * The symbol number of the terminal or non terminal being represented
+     */
+    public int sym;
 
   /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
-  /** The parse state to be recorded on the parse stack with this symbol.
-   *  This field is for the convenience of the parser and shouldn't be 
-   *  modified except by the parser. 
-   */
-  public int parse_state;
-  /** This allows us to catch some errors caused by scanners recycling
-   *  symbols.  For the use of the parser only. [CSA, 23-Jul-1999] */
-  boolean used_by_parser = false;
+    /**
+     * The parse state to be recorded on the parse stack with this symbol.
+     * This field is for the convenience of the parser and shouldn't be
+     * modified except by the parser.
+     */
+    public int parse_state;
+    /**
+     * This allows us to catch some errors caused by scanners recycling
+     * symbols.  For the use of the parser only. [CSA, 23-Jul-1999]
+     */
+    boolean used_by_parser = false;
 
-/*******************************
-  The data passed to parser
- *******************************/
+    /*******************************
+     * The data passed to parser
+     *******************************/
 
-  public int left, right;
-  public Object value;
+    public int left, right;
+    public Object value;
 
-  /*****************************
-    Printing this token out. (Override for pretty-print).
-    ****************************/
-  public String toString() { return "#"+sym; }
+    /*****************************
+     * Printing this token out. (Override for pretty-print).
+     ****************************/
+    public String toString() {
+        return "#" + sym;
+    }
 }
 
 
